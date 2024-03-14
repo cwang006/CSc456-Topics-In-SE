c51fd88915fea1d991756d058c7e6d8e75855863
KevinZheng0701 <kevinzheng700@gmail.com>
2024-03-14 01:08:34 -0400

    testing

--- videogame/samples/index.js
+++ videogame/samples/index.js
@@ -1,29 +1,28 @@
-const mongoose = require('mongoose');
-const videoGame = require('../models/games');
-const seedGames = require('./seedGame');
-mongoose.connect('mongodb://localhost:27017/video-game', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+const mongoose = require("mongoose");
+const videoGame = require("../models/games");
+const seedGames = require("./seedGame");
+mongoose.connect("mongodb://localhost:27017/video-game", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
 const seedDB = async () => {
-    await videoGame.deleteMany({});
+  await videoGame.deleteMany({});
 
-    for (let gameData of seedGames) {
-        const game = new videoGame(gameData);
-        await game.save();
-    }
+  for (let gameData of seedGames) {
+    const game = new videoGame(gameData);
+    await game.save();
+  }
 
-    console.log("Database seeded with sample video games.");
+  console.log("Database seeded with sample video games.");
 };
 
 seedDB().then(() => {
-    mongoose.connection.close();
+  mongoose.connection.close();
 });
