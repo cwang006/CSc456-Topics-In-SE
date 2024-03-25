76513bc38ecca77b038c28bb3126f48daad12085
ChunTing Wang <117398079+cwang006@users.noreply.github.com>
2024-03-25 13:02:26 -0400

    Update info.js

--- RAWG_API/info.js
+++ RAWG_API/info.js
@@ -2,14 +2,37 @@
 const API_KEY = "";
 const API_URL = "https://api.rawg.io/api/games?key=" + API_KEY;
 
-async function fetchData(URL){
-    try{
-        const result = await fetch(API_URL);
-        const games = await result.json();
-        console.log(games);
+async function fetchAPIData(URL){
+    try {
+        const response = await fetch(URL);
+        if(!response.ok){
+            throw new Error('Bad Network Response');
+        }
 
-    } 
-    catch (error){
-        console.log('Error fetching data:', error);
+        const data = await response.json();
+        const games_Listed = data.results; 
+        games_Listed.forEach(game => {
+            handleGameData(game); 
+            // Pass each game to handleGameData
+        });
+
+    } catch (error){
+        console.error('Error: ', error);
     }
 }
+
+function handleGameData(game){
+    const game_id = game.id;
+    const game_name = game.name;
+    const release_date = game.released;
+    const tags = game.tags.map(tag => tag.name);
+    
+    console.log(`Id: ${game_id}`);
+    console.log(`Name: ${game_name}`);
+    console.log(`Released: ${release_date}`);
+    console.log(`Tags: ${tags}`);
+    console.log("\n");
+}
+
+//fetchAPIData(API_URL);
+module.exports = { fetchAPIData, handleGameData };
