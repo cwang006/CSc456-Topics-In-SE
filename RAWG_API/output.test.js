bd78e0db83d47d97b70f5488a8bf52b2bd7ccde0
ChunTing Wang <117398079+cwang006@users.noreply.github.com>
2024-03-25 13:02:51 -0400

    Update output.test.js

--- RAWG_API/output.test.js
+++ RAWG_API/output.test.js
@@ -1,14 +1,28 @@
-import { fetchData } from "./info";
+const { fetchAPIData, handleGameData } = require('./info')
+const API_URL = "https://api.rawg.io/api/games?key=8ad08da8555a47329f90c369942bb69e";
 
-//Expected format of output 
-global.fetch = jest.fn(() =>
-    Promise.resolve({
-        json: () => Promise.resolve({ key: 'value' }), 
-    })
-);
+test('checking server response', async () => {
+    const response = await fetchAPIData(API_URL);
+    // Check if the response is not null
+    expect(response).not.toBeNull();
+});
 
-it("returns a JSON response", async () => {
-    const returned_data = await fetchData("https://api.rawg.io/api/games?key=8ad08da8555a47329f90c369942bb69e");
-    const f = jest.fn()
-    expect(f).toHaveBeenCalledWith(returned_data)
-})
+test('verifying API key response since you need a key to run', async () =>{
+    const response = await fetchAPIData('https://api.rawg.io/api/games');
+    expect(response).toBeUndefined();
+});
+
+test('testing handleGameData output', () => {
+    console.log = jest.fn();
+    const mockGameData = {
+        id: 1, 
+        name : 'Game 1', 
+        released : '2000-01-01',
+        tags : [{ name : 'tag1' }, { name : 'tag2' }] 
+    }
+    handleGameData(mockGameData);
+    expect(console.log).toHaveBeenCalledWith(`Id: ${mockGameData.id}`);
+    expect(console.log).toHaveBeenCalledWith(`Name: ${mockGameData.name}`);
+    expect(console.log).toHaveBeenCalledWith(`Released: ${mockGameData.released}`);
+    expect(console.log).toHaveBeenCalledWith(expect.stringContaining('Tags:'));
+});
